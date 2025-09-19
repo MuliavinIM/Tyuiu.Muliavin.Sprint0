cded3b9ed6296bba17d05ccc9c78752b222b19f4
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-09-19 21:12:15 +0500

    Task3

---
+++ Tyuiu.MuliavinIM.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿
+using Tyuiu.MuliavinIM.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.MuliavinIM.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.MuliavinIM.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.MuliavinIM.Sprint0.Task3.V0.Lib;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7, 7));
+        Console.ReadKey();
+    }
+}
