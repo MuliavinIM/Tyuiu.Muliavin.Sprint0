973d52ad380c6742f330ad6496f79123a3a71d64
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-09-19 20:11:05 +0500

    Task1

---
+++ Tyuiu.Muliavin.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Иван";
+        int age = 17;
+        Console.WriteLine("Hello my name is " + name +"");
+        Console.ReadKey();
+    }
+}
