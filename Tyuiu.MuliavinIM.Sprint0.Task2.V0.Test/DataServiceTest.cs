c553309cc752f5077cb2284203fe1b2b52cc3cb7
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-09-19 20:52:38 +0500

    Task2

---
+++ Tyuiu.MuliavinIM.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MuliavinIM.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MuliavinIM.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageVaild()
+        {
+            var name = "Иван";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Иван", res);
+        }
+    }
+}
